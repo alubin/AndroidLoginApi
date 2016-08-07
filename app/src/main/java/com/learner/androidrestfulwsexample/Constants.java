6fd2983e83b3e814d008b701e18db28082ba230a
AL <andylubin@gmail.com>
2016-08-07 07:45:02 -0400

    Initial commit

---
+++ app/src/main/java/com/learner/androidrestfulwsexample/Constants.java
@@ -0,0 +1,13 @@
+package com.learner.androidrestfulwsexample;
+
+/**
+ * Created by AL on 7/30/16.
+ */
+
+public class Constants {
+
+    public static String ipAddress = "192.168.1.29";
+    public static String serviceName = "RestfulUsers";
+
+    public static String webServiceAddress = "http://"+ipAddress+":8080/"+serviceName+"/";
+}
---
+++ app/src/main/java/com/learner/androidrestfulwsexample/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.learner.androidrestfulwsexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ *
+ * Home Screen Activity
+ */
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Displays Home Screen
+        setContentView(R.layout.home);
+    }
+
+    public void sendMsg(String txt)
+    {
+
+    }
+
+}
